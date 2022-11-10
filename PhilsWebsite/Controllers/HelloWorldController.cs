6f9d049d075758e696a2ee5a19e02a17e431d49d
phillipgoska <pgoska15@southhills.edu>
2022-11-10 11:02:50 -0500

    Initial commit

---
+++ PhilsWebsite/Controllers/HelloWorldController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace PhilsWebsite.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //GET: /HelloWorld/
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //Get: /HelloWorld/Welcome/
+        //reuires using System.Text.Encodings.Web;
+
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello World" + name;
+            ViewData["Numtimes"] = numTimes;
+            return View();
+        }
+    }
+}

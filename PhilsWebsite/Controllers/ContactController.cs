fa9736bfec8a2d56ab65d277b242f2eb204c4fc1
phillipgoska <pgoska15@southhills.edu>
2022-11-18 16:16:01 -0500

    contact controller

---
+++ PhilsWebsite/Controllers/ContactController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using FluentEmail.Smtp;
+using System.Net.Mail;
+using FluentEmail.Core;
+
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PhilsWebsite.Controllers
+{
+    public class ContactController : Controller
+    {
+        class Contact
+        {
+            static async Task Main(string[] args)
+            {
+                var sender = new SmtpSender(() => new SmtpClient("localhost")
+                {
+                    //by default you wannt SSL on
+                    //testing purpose i will turn it off
+                    //not production
+                    //for demo purposes
+                    EnableSsl = false,
+                    DeliveryMethod = SmtpDeliveryMethod.SpecifiedPickupDirectory,
+                    //when you send an email it is sent to a directory as a file
+                    PickupDirectoryLocation = @"C:\Demos"
+                    
+                });
+                
+                Email.DefaultSender = sender;
+
+                //creating an email
+                var email = await Email
+                    .From("phil@gmail.com")
+                    .To("test@test.com", "Sue")
+                    .Subject("Thanks")
+                    .Body("Thanks for leaving a message")
+                    .SendAsync();
+
+                
+            }
+        }
+    }
+
+}
+
